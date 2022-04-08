ab4bcea772542d2f62f43b9f7aa0deb326e982f7
Nicolas Gatien <49788382+Nicolas-Gatien@users.noreply.github.com>
2022-04-07 23:38:47 -0400

    Added Comments

--- imageTexter.py
+++ imageTexter.py
@@ -1,47 +1,52 @@
 from tokenize import String
 from PIL import Image
 from math import sqrt
-imag = Image.open("Image.jpg")
-#Convert the image te RGB if it is a .gif for example
+
+# Get Image
+imag = Image.open("Image.jpg") 
 imag = imag.convert ('RGB')
-#coordinates of the pixel
 
 console_width = 287
 console_height = 9999
 
+# Resize Image to fit in console
 while (imag.height > console_height):
     imag = imag.resize(((int)(imag.width * 0.95), (int)(imag.height * 0.95)))
 
 while (imag.width > console_width):
     imag = imag.resize(((int)(imag.width * 0.95), (int)(imag.height * 0.95)))
 
+# For each Row
 for y in range(imag.height):
     output = ""
+    # Loop through each pixel in that row
     for x in range(imag.width):
-        #Get RGB
-        pixelRGB = imag.getpixel((x,y))
+        pixelRGB = imag.getpixel((x,y)) # Get Pixel info
         R,G,B = pixelRGB 
         brightness = sqrt(0.299*(R**2) + 0.587*(G**2) + 0.114*(B**2)) ##0 is dark (black) and 255 is bright (white)
+
+        # Loop through each pixel in the row
         x += 1
-        if R - 75 > G and R - 75 > B:
+        if R - 75 > G and R - 75 > B: # If it is mostly Red, set it red
             output += "\033[1;31;40m"
-        elif G - 75 > R and G - 75 > B:
+        elif G - 75 > R and G - 75 > B: # Mostly Green, set it green
             output += "\033[1;32;40m"
-        elif B - 75 > R and B - 75 > G:
+        elif B - 75 > R and B - 75 > G: # Mostly Blue, set it blue
             output += "\033[1;34;40m"
-        elif R > B and G > B:
+        elif R > B and G > B: # Colour Mixes
             output += "\033[1;33;40m"
         elif R > G and B > G:
             output += "\033[1;35;40m"
         elif G > R and B > R:
             output+= "\033[1;36;40m"
-        elif R > G and R > B:
+        elif R > G and R > B: # Default Colours
             output += "\033[1;31;40m"
         elif G > R and G > B:
             output += "\033[1;32;40m"
         elif B > R and B > G:
             output += "\033[1;34;40m"
 
+        # Determine the Character that will be written based on brightness of pixel
         if brightness >= 224:
             output += "@@"
         elif brightness >= 208:
@@ -72,6 +77,8 @@ for y in range(imag.height):
             output += ".."
         else:
             output += "  "
-        
-    y += 1
+
+    # Print Row
     print(f"{output}")
+    # Next Row
+    y += 1
